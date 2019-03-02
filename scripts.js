e49d38ba8f6105ba825cf3184184ace29a2965f5
Rick <3741326+RickJP@users.noreply.github.com>
2019-03-02 21:20:02 +0900

    handles edge cases (single card double-clicked stays face up; allowed three or more cards to be turned over at once) by locking board

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-    // console.log('Clicked');
-    // console.log(this);
+
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
     this.classList.toggle('flip');
 
     if (!hasFlippedCard) {
@@ -23,14 +26,25 @@ function flipCard() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unflipCards() {
+    lockBoard = true;
+    
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1000);
     
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 function checkForMatch() {
