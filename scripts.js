e47e4959c6bbc2713d06869a177b90a00e202b69
Rick <3741326+RickJP@users.noreply.github.com>
2019-03-02 20:23:26 +0900

    added card flip effect

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const cards = document.querySelectorAll('.memory-card');
+
+function flipCard() {
+    console.log('Clicked');
+    console.log(this);
+    this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
