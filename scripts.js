2ddc596f07f81cb016e2dac995908bc5b94e62c7
Rick <3741326+RickJP@users.noreply.github.com>
2019-03-02 20:47:23 +0900

    matched cards kept, unmatched cards flipped back

--- scripts.js
+++ scripts.js
@@ -1,9 +1,38 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    console.log('Clicked');
-    console.log(this);
+    // console.log('Clicked');
+    // console.log(this);
     this.classList.toggle('flip');
+
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+
+        // do card matching
+        if (firstCard.dataset.framework === 
+            secondCard.dataset.framework) {
+             // matched
+             firstCard.removeEventListener('click', flipCard);
+             secondCard.removeEventListener('click', flipCard);
+        } else  {
+            // not matched
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            }, 1000);
+            
+        }
+        
+    }
+
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
