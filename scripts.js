ce5501ff76a1cdf1d1eaaf71ce0ea3b1588efbd3
Rick <3741326+RickJP@users.noreply.github.com>
2019-03-02 20:56:18 +0900

    refactored code for brevity and clarity

--- scripts.js
+++ scripts.js
@@ -12,27 +12,32 @@ function flipCard() {
         // first click
         hasFlippedCard = true;
         firstCard = this;
-    } else {
-        hasFlippedCard = false;
-        secondCard = this;
+        return;
+    } 
+    hasFlippedCard = false;
+    secondCard = this;
 
-        // do card matching
-        if (firstCard.dataset.framework === 
-            secondCard.dataset.framework) {
-             // matched
-             firstCard.removeEventListener('click', flipCard);
-             secondCard.removeEventListener('click', flipCard);
-        } else  {
-            // not matched
-            setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 1000);
-            
-        }
-        
-    }
+    checkForMatch();
+}
 
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1000);
+    
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework ===
+        secondCard.dataset.framework;
+
+    isMatch ? disableCards() : unflipCards();
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
